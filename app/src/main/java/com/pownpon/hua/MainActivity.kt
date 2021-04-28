a131df5573ddbad5c3c4e070632bf73c5f2e2a26
pownpon <337136486@qq.com>
2021-04-28 16:49:57 +0800

    1

--- app/src/main/java/com/pownpon/hua/MainActivity.kt
+++ app/src/main/java/com/pownpon/hua/MainActivity.kt
@@ -2,10 +2,14 @@ package com.pownpon.hua
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        thread {
+            
+        }
     }
 }
