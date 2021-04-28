c19b9ed9d82fca1f87cb1f9cd4476fc2642afe7b
pownpon <337136486@qq.com>
2021-04-28 16:51:14 +0800

    1

--- app/src/main/java/com/pownpon/hua/MainActivity.kt
+++ app/src/main/java/com/pownpon/hua/MainActivity.kt
@@ -2,14 +2,16 @@ package com.pownpon.hua
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlin.concurrent.thread
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         thread {
-            
+            Log.e("ss","ss")
         }
     }
 }
